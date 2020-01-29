80917de31f511edf529322b4e78f3824815806c6
codingbyjames <codingbyjames@gmail.com>
2020-01-29 12:44:19 -0800

    modifies animation of label to use CLTypingLabel

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,27 +7,16 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //animation for entry view
-        titleLabel.text = ""
-        var charIndex = 0.0
-        
-        let titleText = "⚡️FlashChat"
-        //for each letter in the title, append letter to the title on a short delay, achieving animated effect.
-        for letter in titleText {
-            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
-                self.titleLabel.text?.append(letter)
-            }
-            charIndex += 1
-        }
-
+        titleLabel.text = "⚡️FlashChat"
        
     }
     
